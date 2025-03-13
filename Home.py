b29f350f881541cd345bf7d66c38321ff61674c2
Sina Bahrami <45111906+sinabahrami@users.noreply.github.com>
2025-03-13 11:03:09 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,7 +19,9 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.markdown("""
-<p class="big-font"> -Electric bus range (the distance an electric bus can travel on a fully charged battery) -Bus energy usage -Stationary and dynamic charging power -Stationary charging setup time (inductive/wireless chargers can start charging immediately, whereas plug-in chargers require a few minutes to connect and begin charging)</p>
+<p class="big-font"> -Electric bus range (the distance an electric bus can travel on a fully charged battery) 
+-Bus energy usage -Stationary and dynamic charging power 
+-Stationary charging setup time (inductive/wireless chargers can start charging immediately, whereas plug-in chargers require a few minutes to connect and begin charging)</p>
 """, unsafe_allow_html=True)
 
 if st.button("Take me to the app"):
