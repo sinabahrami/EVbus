66d01a144136e377c32d5c719811b744d189ee6a
Sina Bahrami <45111906+sinabahrami@users.noreply.github.com>
2025-03-13 10:37:41 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 st.title("Welcome!")
-st.write("This app has been developed by a team of researchers from the University of Michigan, including [Prof. Yafeng Yin](https://cee.engin.umich.edu/people/yin-yafeng/), [Dr. Sina Bahrami](https://sinabahrami.github.io/), and Dr. Manzi Li, as part of the deliverables for the MDOT (Michigan Department of Transportation) research project OR24-003: \"Inductive Vehicle Charging - Identify Best Practices/Applications and Optimum Locations for Light to Heavy Duty Vehicles\".")
+st.markdown("""
+<style>
+.big-font {
+    font-size: 16px !important;
+    line-height: 1.5; /* Adjusts line spacing */
+    }
+</style>
+<p class="big-font">This app has been developed by a team of researchers from the University of Michigan, including [Prof. Yafeng Yin](https://cee.engin.umich.edu/people/yin-yafeng/), [Dr. Sina Bahrami](https://sinabahrami.github.io/), and Dr. Manzi Li, as part of the deliverables for the MDOT (Michigan Department of Transportation) research project OR24-003: \"Inductive Vehicle Charging - Identify Best Practices/Applications and Optimum Locations for Light to Heavy Duty Vehicles\".</p>
+""", unsafe_allow_html=True)
+#st.write("")
 st.write("This web app is designed to assist transit agencies and policymakers in evaluating the transition to an electric fleet. It utilizes [GTFS (General Transit Feed Specification)](https://gtfs.org/getting-started/what-is-GTFS/) data along with user inputs, including:")
 
 if st.button("Take me to the app"):
