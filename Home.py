f77eab987a3cacc8cc3d86a04c0ed31337b90487
Sina Bahrami <45111906+sinabahrami@users.noreply.github.com>
2025-03-13 10:49:22 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,8 +10,18 @@ st.markdown("""
 </style>
 <p class="big-font">This app has been developed by a team of researchers from the University of Michigan, including <a href="https://cee.engin.umich.edu/people/yin-yafeng/"> Prof. Yafeng Yin</a>, <a href="https://sinabahrami.github.io/"> Dr. Sina Bahrami</a>, and Dr. Manzi Li, as part of the deliverables for the MDOT (Michigan Department of Transportation) research project <strong>OR24-003</strong>: \"Inductive Vehicle Charging - Identify Best Practices/Applications and Optimum Locations for Light to Heavy Duty Vehicles\".</p>
 """, unsafe_allow_html=True)
-#st.write("")
-st.write("This web app is designed to assist transit agencies and policymakers in evaluating the transition to an electric fleet. It utilizes [GTFS (General Transit Feed Specification)](https://gtfs.org/getting-started/what-is-GTFS/) data along with user inputs, including:")
+
+
+st.markdown("""
+<style>
+.big-font {
+    font-size: 20px !important;
+    line-height: 2; /* Adjusts line spacing */
+    }
+</style>
+<p class="big-font">This web app is designed to assist transit agencies and policymakers in evaluating the transition to an electric fleet. It utilizes <a href="https://gtfs.org/getting-started/what-is-GTFS/> GTFS (General Transit Feed Specification)</a> data along with user inputs, including:\".</p>
+""", unsafe_allow_html=True)
+
 
 if st.button("Take me to the app"):
     st.switch_page("pages/app.py")  
