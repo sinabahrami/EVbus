0ba4bb6ffaa8051ccd56354728ccf7953ccf8cbb
Sina Bahrami <45111906+sinabahrami@users.noreply.github.com>
2025-03-17 10:37:06 -0400

    Update Block.py

--- pages/Block.py
+++ pages/Block.py
@@ -344,7 +344,9 @@ def main():
         # Allow user to select agency
         selected_agency = st.selectbox("Select the agency", agencies)
 
-        if selected_agency=="UMich":
+        if selected_agency=="BATA (Traverse City)"
+            feasible_options=[424160, 424161, 424162, 424163, 424164, 424165, 424166, 424167, 424168, 424169, 424170, 424171, 424172, 424173, 424174, 424175, 424176, 424177, 424178, 424179, 424180, 424181, 424182, 424183, 424184, 424185, 424186, 424187]
+        elif selected_agency=="UMich":
             feasible_options =['BB217', 'CN100', 'CN101', 'CN102', 'CN103', 'CN104', 'CN105', 'CN106', 'CN107', 'CN108', 'CN109', 'CN110', 'CS100', 'CS101', 'CS102', 'CS103', 'CS104', 'CS105', 'CSX550', 'CSX552', 'MX500', 'MX501', 'MX502', 'MX503', 'MX504', 'MX505', 'MX506', 'MX507', 'MX508', 'MX509', 'MX510', 'MX511', 'NES700', 'NES701', 'NES702', 'NES703', 'NW351', 'NW352', 'NW353', 'NW354', 'NW355', 'NW356', 'NW357', 'NW358', 'OS260', 'OS261', 'OS262', 'OS263', 'OS264', 'T1', 'T2', 'WS600', 'WS601', 'WS602', 'WS603', 'WS604', 'WX600', 'WX601']
         
         user_choice = st.multiselect("Choose desired blocks:", feasible_options)        
@@ -596,7 +598,6 @@ def main():
 
                 # Filter to include only block_id that are in user choice
                 block_general = block_general[block_general['block_id'].isin(user_choice)]
-                st.write(f"{block_general}")
                 
                 block_general['time_gaps_sum'] = block_general['time_gaps'].apply(lambda x: np.nansum(x) if isinstance(x, list) else np.nan)
                 block_general["time_gaps"] = block_general["time_gaps"].apply(lambda lst: [x for x in lst if not pd.isna(x)])
