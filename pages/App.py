a986abbc4858178cdec5d44b8c5d99be241e2e9d
Sina Bahrami <45111906+sinabahrami@users.noreply.github.com>
2025-08-14 12:18:28 -0400

    Update App.py

--- pages/App.py
+++ pages/App.py
@@ -930,7 +930,7 @@ def main():
                     top_end_stop_ids.remove(id)  # Remove safely
 
                     block_general["range_tracking"] = block_general.apply(
-                        lambda row: compute_range_tracking_lane(row["distances_list"], row["time_gaps"], row["end_id_list"], row["trips_by_route"], row["avg_speed_list"],bus_range, charging_power,dynamic_wireless_charging_power, energy_usage, min_stoppage_time, top_end_stop_ids, wireless_track_shapeids,wireless_track_shape,shapes), if row["total_distance_miles"] > bus_range and id in row["end_id_list"] else row["range_tracking"],
+                        lambda row: compute_range_tracking_lane(row["distances_list"], row["time_gaps"], row["end_id_list"], row["trips_by_route"], row["avg_speed_list"],bus_range, charging_power,dynamic_wireless_charging_power, energy_usage, min_stoppage_time, top_end_stop_ids, wireless_track_shapeids,wireless_track_shape,shapes) if row["total_distance_miles"] > bus_range and id in row["end_id_list"] else row["range_tracking"],
                         axis=1
                     )
                     
@@ -1115,3 +1115,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
