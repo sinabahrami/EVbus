bd6bebc63bdd27917e3ed6322b0e8542cfc78ead
Sina Bahrami <45111906+sinabahrami@users.noreply.github.com>
2025-08-20 16:00:39 -0400

    Update App.py

--- pages/App.py
+++ pages/App.py
@@ -539,7 +539,7 @@ class FullPageImage(Flowable):
                 
         # Starting Y position from bottom
         yt=self.top_margin+600
-        yb = self.bottom_margin-20
+        yb = self.bottom_margin-30
 
         
         # Draw title above subtitle
@@ -559,8 +559,8 @@ class FullPageImage(Flowable):
                 self.canv.drawCentredString(self.width / 2, yb, line)
                 yb -= self.subtitle_font_size + 3  # spacing between lines
         if self.licence:
-            yb-=50
-            self.canv.setFont("Times-Bold", 10)
+            yb-=40
+            self.canv.setFont("Times-Roman", 10)
             self.canv.setFillColorRGB(0, 0, 0)
             self.canv.drawCentredString(self.width / 2, yb, self.licence)
             
@@ -1796,6 +1796,7 @@ def main():
         
 if __name__ == "__main__":
     main()
+
 
 
 
